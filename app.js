d145d9212ba7b8846c70f73c5dcad2ebff681192
sideeq12 <abdwaheed2018@gmail.com>
2023-03-15 15:56:22 +0100

    layout

--- app.js
+++ app.js
@@ -7,10 +7,46 @@ let headname = document.querySelector(".headline");
 let message = document.querySelector(".message")
 let label = document.querySelector(".lab")
 let home = document.querySelector(".firstContainer")
-
 let button = document.querySelector(".explore")
+
+gsap.from(firstImage, {
+    y: -200,
+    opacity : 0,
+    duration : 1,
+    ease : "power2.inOut"
+})
+gsap.from(secondImage, {
+    y: -200,
+    opacity : 0,
+    duration : 1,
+    delay : 0.5,
+    ease : "power2.inOut"
+})
+gsap.from(headname, {
+    x: 200,
+    opacity : 0,
+    duration : 1,
+    delay : 1,
+    scale : 0,
+    ease : "power2.inOut"
+})
+gsap.from(message, {
+    x: 200,
+    opacity : 0,
+    duration : 1,
+    scale : 0,
+    delay : 1.5,
+    ease : "power2.inOut"
+})
+gsap.from(button, {
+    x: 200,
+    opacity : 0,
+    duration : 1,
+    scale : 0,
+    delay : 2,
+    ease : "power2.inOut"
+})
 button.addEventListener("click", ()=>{
-    console.log("the bbt works")
     gsap.to(firstImage, {
         y: -200,
         duration : 1,
@@ -42,26 +78,12 @@ button.addEventListener("click", ()=>{
         y: -200,
         duration : 1,
         ease : "power2.inOut",
-        delay : 1,
-        backgroundColor : 'rgba(0, 0, 0, 0.01)',
-        border : "3px dashed yellow"
+        display : "none",
+        delay : 1.3
     })
     gsap.to(label, {
         opacity : 0,
         delay: 1.5
-    })
-    gsap.to(button, {
-        width : "200px",
-        height : "200px",
-        borderRadius : "+=100%",
-        delay : 1.7,
-    })
-    gsap.to(button, {
-        rotation: 360,
-        scale : 20,
-        duration : 3,
-        opacity : 0,
-        delay: 2
     })
     gsap.to(home, {
         display:"none",
