0b194d07db48b8f721d578984d1285ebc6463f8f
sideeq12 <abdwaheed2018@gmail.com>
2023-03-15 14:39:54 +0100

    all line

--- app.js
+++ app.js
@@ -69,15 +69,15 @@ button.addEventListener("click", ()=>{
     })
 })
 
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".container",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    end: () => "+=" + document.querySelector(".container").offsetWidth
-  }
-});
+// gsap.to(sections, {
+//   xPercent: -100 * (sections.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".container",
+//     pin: true,
+//     scrub: 1,
+//     snap: 1 / (sections.length - 1),
+//     end: () => "+=" + document.querySelector(".container").offsetWidth
+//   }
+// });
 
